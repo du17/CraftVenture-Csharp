67e956762b0976c4621deb5e54894ff3fc980230
Luan <luanhenricoa@gmail.com>
2023-10-18 14:14:00 -0300

    Login e cadastro feitos mas sem nenhuma ação apenas as telas.

---
+++ GerenciadorDeEstoque/Apresentação/frmCadastro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciadorDeEstoque.Apresentação
+{
+    public partial class frmCadastro : Form
+    {
+        public frmCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNome_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            lblNome.Visible = false;
+        }
+
+        private void txtEmail_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            lblEmail.Visible = false;
+        }
+
+        private void txtSenha_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            lblSenha.Visible = false;
+        }
+
+        private void txtRepetirSenha_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            lblRepetirSenha.Visible = false;
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ GerenciadorDeEstoque/Apresentação/frmLogin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace GerenciadorDeEstoque.Apresentação
+{
+    public partial class frmLogin : KryptonForm
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNome_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            lblNome.Visible = false;
+        }
+
+        private void txtSenha_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            lblSenha.Visible = false;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
