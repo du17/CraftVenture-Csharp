c3edc5be553f7712b8f43055529557645e9aa372
Eduarda Machado Carreira <dumachado1755@gmail.com>
2023-10-25 14:20:34 -0300

    commit DAO

--- GerenciadorDeEstoque/DAO/DAO.cs
+++ GerenciadorDeEstoque/DAO/DAO.cs
@@ -90,7 +90,7 @@ namespace GerenciadorDeEstoque.DAO
         #endregion
 
         #region Cliente
-        public void IDC(String email, String nome, String senha)
+        /*public void IDC(String email, String nome, String senha)
         {
             con = new MySqlConnection();
             conexao = new Conexao();
@@ -159,8 +159,86 @@ namespace GerenciadorDeEstoque.DAO
             {
                 con.Close();
             }
-            #endregion
+
+        }*/
+
+        #endregion
+
+        #region Pedido
+
+        /*public void IDP(String email, String nome, String senha)
+        {
+            con = new MySqlConnection();
+            conexao = new Conexao();
+            con.ConnectionString = conexao.getConnectionString();
+
+            String query = "INSERT INTO pedido (email, nome, senha) VALUES";
+            query += "(?email, ?nome, ?senha)";
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.Parameters.AddWithValue("?email", email);
+                cmd.Parameters.AddWithValue("?nome", nome);
+                cmd.Parameters.AddWithValue("?senha", senha);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void ADP(String email, String nome, String senha, Int32 itemid)
+        {
+            con = new MySqlConnection();
+            conexao = new Conexao();
+            con.ConnectionString = conexao.getConnectionString();
+            String query = "UPDATE estoque SET email = ?email, senha = ?senha, nome = ?nome";
+            query += " WHERE itemid = ?itemid";
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.Parameters.AddWithValue("?nome", nome);
+                cmd.Parameters.AddWithValue("?email", email);
+                cmd.Parameters.AddWithValue("?senha", senha);
+                cmd.Parameters.AddWithValue("?itemid", itemid);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+            }
+            finally
+            {
+                con.Close();
+            }
 
         }
+
+        public void RDP(String nome, Int32 itemid)
+        {
+            con = new MySqlConnection();
+            conexao = new Conexao();
+            con.ConnectionString = conexao.getConnectionString();
+            String query = "DELETE FROM estoque";
+            query += "WHERE nome = ?nome AND itemid = ?itemid";
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.Parameters.AddWithValue("?nome", nome);
+                cmd.Parameters.AddWithValue("?itemid", itemid);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }*/
+
+        #endregion
+
     }
 }
