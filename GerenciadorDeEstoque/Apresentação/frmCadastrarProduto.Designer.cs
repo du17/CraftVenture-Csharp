ef8399264d98e8634d1454d5c5edbd3604d8cd65
VitorSkull <vitorskull46@gmail.com>
2023-11-14 01:16:14 -0300

    Tela "Cadastrar Produto" criada, falta os textboxs que provavavelmente o luan vai adicionar depois

--- GerenciadorDeEstoque/Apresentação/frmCadastrarProduto.Designer.cs
+++ GerenciadorDeEstoque/Apresentação/frmCadastrarProduto.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GerenciadorDeEstoque
 {
-    partial class frmVenda
+    partial class frmCadastrarProduto
     {
         /// <summary>
         /// Required designer variable.
@@ -28,14 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVenda));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCadastrarProduto));
             this.btnCadastro = new System.Windows.Forms.Button();
             this.btnEstoque = new System.Windows.Forms.Button();
             this.btnVenda = new System.Windows.Forms.Button();
             this.btnHistórico = new System.Windows.Forms.Button();
             this.btnSair = new System.Windows.Forms.Button();
             this.btnAdicionarProdutos = new ComponentFactory.Krypton.Toolkit.KryptonButton();
-            this.btmConfirmar = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kryptonButton2 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
             this.SuspendLayout();
             // 
             // btnCadastro
@@ -50,7 +51,7 @@
             this.btnCadastro.Location = new System.Drawing.Point(7, 126);
             this.btnCadastro.Name = "btnCadastro";
             this.btnCadastro.Size = new System.Drawing.Size(250, 88);
-            this.btnCadastro.TabIndex = 54;
+            this.btnCadastro.TabIndex = 55;
             this.btnCadastro.UseVisualStyleBackColor = false;
             // 
             // btnEstoque
@@ -65,7 +66,7 @@
             this.btnEstoque.Location = new System.Drawing.Point(-5, 221);
             this.btnEstoque.Name = "btnEstoque";
             this.btnEstoque.Size = new System.Drawing.Size(250, 88);
-            this.btnEstoque.TabIndex = 55;
+            this.btnEstoque.TabIndex = 56;
             this.btnEstoque.UseVisualStyleBackColor = false;
             // 
             // btnVenda
@@ -80,7 +81,7 @@
             this.btnVenda.Location = new System.Drawing.Point(-26, 323);
             this.btnVenda.Name = "btnVenda";
             this.btnVenda.Size = new System.Drawing.Size(250, 88);
-            this.btnVenda.TabIndex = 56;
+            this.btnVenda.TabIndex = 57;
             this.btnVenda.UseVisualStyleBackColor = false;
             // 
             // btnHistórico
@@ -95,7 +96,7 @@
             this.btnHistórico.Location = new System.Drawing.Point(5, 411);
             this.btnHistórico.Name = "btnHistórico";
             this.btnHistórico.Size = new System.Drawing.Size(250, 88);
-            this.btnHistórico.TabIndex = 57;
+            this.btnHistórico.TabIndex = 58;
             this.btnHistórico.UseVisualStyleBackColor = false;
             // 
             // btnSair
@@ -110,16 +111,16 @@
             this.btnSair.Location = new System.Drawing.Point(-41, 506);
             this.btnSair.Name = "btnSair";
             this.btnSair.Size = new System.Drawing.Size(250, 88);
-            this.btnSair.TabIndex = 58;
+            this.btnSair.TabIndex = 59;
             this.btnSair.UseVisualStyleBackColor = false;
             // 
             // btnAdicionarProdutos
             // 
             this.btnAdicionarProdutos.ButtonStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.LowProfile;
             this.btnAdicionarProdutos.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAdicionarProdutos.Location = new System.Drawing.Point(285, 606);
+            this.btnAdicionarProdutos.Location = new System.Drawing.Point(495, 504);
             this.btnAdicionarProdutos.Name = "btnAdicionarProdutos";
-            this.btnAdicionarProdutos.Size = new System.Drawing.Size(505, 66);
+            this.btnAdicionarProdutos.Size = new System.Drawing.Size(505, 44);
             this.btnAdicionarProdutos.StateCommon.Back.Color1 = System.Drawing.Color.Transparent;
             this.btnAdicionarProdutos.StateCommon.Back.Color2 = System.Drawing.Color.Transparent;
             this.btnAdicionarProdutos.StateCommon.Back.Draw = ComponentFactory.Krypton.Toolkit.InheritBool.True;
@@ -148,73 +149,114 @@
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
             this.btnAdicionarProdutos.StateTracking.Border.Rounding = 7;
-            this.btnAdicionarProdutos.TabIndex = 60;
+            this.btnAdicionarProdutos.TabIndex = 61;
             this.btnAdicionarProdutos.Values.Text = "";
             // 
-            // btmConfirmar
+            // kryptonButton1
             // 
-            this.btmConfirmar.ButtonStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.LowProfile;
-            this.btmConfirmar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btmConfirmar.Location = new System.Drawing.Point(850, 607);
-            this.btmConfirmar.Name = "btmConfirmar";
-            this.btmConfirmar.Size = new System.Drawing.Size(389, 66);
-            this.btmConfirmar.StateCommon.Back.Color1 = System.Drawing.Color.Transparent;
-            this.btmConfirmar.StateCommon.Back.Color2 = System.Drawing.Color.Transparent;
-            this.btmConfirmar.StateCommon.Back.Draw = ComponentFactory.Krypton.Toolkit.InheritBool.True;
-            this.btmConfirmar.StateCommon.Back.Image = ((System.Drawing.Image)(resources.GetObject("btmConfirmar.StateCommon.Back.Image")));
-            this.btmConfirmar.StateCommon.Back.ImageAlign = ComponentFactory.Krypton.Toolkit.PaletteRectangleAlign.Local;
-            this.btmConfirmar.StateCommon.Back.ImageStyle = ComponentFactory.Krypton.Toolkit.PaletteImageStyle.CenterMiddle;
-            this.btmConfirmar.StateCommon.Border.Color1 = System.Drawing.Color.Transparent;
-            this.btmConfirmar.StateCommon.Border.Color2 = System.Drawing.Color.Transparent;
-            this.btmConfirmar.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            this.kryptonButton1.ButtonStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.LowProfile;
+            this.kryptonButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kryptonButton1.Location = new System.Drawing.Point(274, 581);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(458, 66);
+            this.kryptonButton1.StateCommon.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateCommon.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateCommon.Back.Draw = ComponentFactory.Krypton.Toolkit.InheritBool.True;
+            this.kryptonButton1.StateCommon.Back.Image = ((System.Drawing.Image)(resources.GetObject("kryptonButton1.StateCommon.Back.Image")));
+            this.kryptonButton1.StateCommon.Back.ImageAlign = ComponentFactory.Krypton.Toolkit.PaletteRectangleAlign.Local;
+            this.kryptonButton1.StateCommon.Back.ImageStyle = ComponentFactory.Krypton.Toolkit.PaletteImageStyle.CenterMiddle;
+            this.kryptonButton1.StateCommon.Border.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateCommon.Border.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.btmConfirmar.StateCommon.Border.Rounding = 7;
-            this.btmConfirmar.StateCommon.Content.Image.ImageColorMap = System.Drawing.Color.Transparent;
-            this.btmConfirmar.StateCommon.Content.Image.ImageColorTo = System.Drawing.Color.Transparent;
-            this.btmConfirmar.StateCommon.Content.Image.ImageV = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Near;
-            this.btmConfirmar.StateNormal.Back.Color1 = System.Drawing.Color.Transparent;
-            this.btmConfirmar.StateNormal.Back.Color2 = System.Drawing.Color.Transparent;
-            this.btmConfirmar.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(189)))), ((int)(((byte)(243)))));
-            this.btmConfirmar.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(189)))), ((int)(((byte)(243)))));
-            this.btmConfirmar.StateTracking.Back.Image = global::GerenciadorDeEstoque.Properties.Resources.LIMPAR;
-            this.btmConfirmar.StateTracking.Back.ImageStyle = ComponentFactory.Krypton.Toolkit.PaletteImageStyle.CenterMiddle;
-            this.btmConfirmar.StateTracking.Border.Color1 = System.Drawing.Color.Transparent;
-            this.btmConfirmar.StateTracking.Border.Color2 = System.Drawing.Color.Transparent;
-            this.btmConfirmar.StateTracking.Border.Draw = ComponentFactory.Krypton.Toolkit.InheritBool.True;
-            this.btmConfirmar.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            this.kryptonButton1.StateCommon.Border.Rounding = 7;
+            this.kryptonButton1.StateCommon.Content.Image.ImageColorMap = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateCommon.Content.Image.ImageColorTo = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateCommon.Content.Image.ImageV = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Near;
+            this.kryptonButton1.StateNormal.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateNormal.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(189)))), ((int)(((byte)(243)))));
+            this.kryptonButton1.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(189)))), ((int)(((byte)(243)))));
+            this.kryptonButton1.StateTracking.Back.Image = global::GerenciadorDeEstoque.Properties.Resources.LIMPAR;
+            this.kryptonButton1.StateTracking.Back.ImageStyle = ComponentFactory.Krypton.Toolkit.PaletteImageStyle.CenterMiddle;
+            this.kryptonButton1.StateTracking.Border.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateTracking.Border.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateTracking.Border.Draw = ComponentFactory.Krypton.Toolkit.InheritBool.True;
+            this.kryptonButton1.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.btmConfirmar.StateTracking.Border.Rounding = 7;
-            this.btmConfirmar.TabIndex = 61;
-            this.btmConfirmar.Values.Text = "";
+            this.kryptonButton1.StateTracking.Border.Rounding = 7;
+            this.kryptonButton1.TabIndex = 62;
+            this.kryptonButton1.Values.Text = "";
             // 
-            // frmVenda
+            // kryptonButton2
+            // 
+            this.kryptonButton2.ButtonStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.LowProfile;
+            this.kryptonButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kryptonButton2.Location = new System.Drawing.Point(761, 579);
+            this.kryptonButton2.Name = "kryptonButton2";
+            this.kryptonButton2.Size = new System.Drawing.Size(458, 66);
+            this.kryptonButton2.StateCommon.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateCommon.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateCommon.Back.Draw = ComponentFactory.Krypton.Toolkit.InheritBool.True;
+            this.kryptonButton2.StateCommon.Back.Image = ((System.Drawing.Image)(resources.GetObject("kryptonButton2.StateCommon.Back.Image")));
+            this.kryptonButton2.StateCommon.Back.ImageAlign = ComponentFactory.Krypton.Toolkit.PaletteRectangleAlign.Local;
+            this.kryptonButton2.StateCommon.Back.ImageStyle = ComponentFactory.Krypton.Toolkit.PaletteImageStyle.CenterMiddle;
+            this.kryptonButton2.StateCommon.Border.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateCommon.Border.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonButton2.StateCommon.Border.Rounding = 7;
+            this.kryptonButton2.StateCommon.Content.Image.ImageColorMap = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateCommon.Content.Image.ImageColorTo = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateCommon.Content.Image.ImageV = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Near;
+            this.kryptonButton2.StateNormal.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateNormal.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(189)))), ((int)(((byte)(243)))));
+            this.kryptonButton2.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(189)))), ((int)(((byte)(243)))));
+            this.kryptonButton2.StateTracking.Back.Image = global::GerenciadorDeEstoque.Properties.Resources.LIMPAR;
+            this.kryptonButton2.StateTracking.Back.ImageStyle = ComponentFactory.Krypton.Toolkit.PaletteImageStyle.CenterMiddle;
+            this.kryptonButton2.StateTracking.Border.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateTracking.Border.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateTracking.Border.Draw = ComponentFactory.Krypton.Toolkit.InheritBool.True;
+            this.kryptonButton2.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonButton2.StateTracking.Border.Rounding = 7;
+            this.kryptonButton2.TabIndex = 63;
+            this.kryptonButton2.Values.Text = "";
+            // 
+            // frmCadastrarProduto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.btmConfirmar);
+            this.Controls.Add(this.kryptonButton2);
+            this.Controls.Add(this.kryptonButton1);
             this.Controls.Add(this.btnAdicionarProdutos);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnHistórico);
             this.Controls.Add(this.btnVenda);
             this.Controls.Add(this.btnEstoque);
             this.Controls.Add(this.btnCadastro);
-            this.Name = "frmVenda";
-            this.Text = "Venda";
+            this.Name = "frmCadastrarProduto";
+            this.Text = "frmCadastrarProduto";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
         private System.Windows.Forms.Button btnCadastro;
         private System.Windows.Forms.Button btnEstoque;
         private System.Windows.Forms.Button btnVenda;
         private System.Windows.Forms.Button btnHistórico;
         private System.Windows.Forms.Button btnSair;
         private ComponentFactory.Krypton.Toolkit.KryptonButton btnAdicionarProdutos;
-        private ComponentFactory.Krypton.Toolkit.KryptonButton btmConfirmar;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton1;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton2;
     }
 }
