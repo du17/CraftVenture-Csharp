f00f0cbec34464a8698d3f6eff01ea33346bfc2a
Eduarda Machado Carreira <dumachado1755@gmail.com>
2023-11-16 22:44:15 -0300

    commit

--- GerenciadorDeEstoque/Apresentação/frmLogin.cs
+++ GerenciadorDeEstoque/Apresentação/frmLogin.cs
@@ -76,40 +76,6 @@ namespace GerenciadorDeEstoque.Apresentação
 
         private void frmLogin_Load(object sender, EventArgs e)
         {
-            /*try
-            {
-                var strConexao = @"server=localhost;uid=root;pwd=root;database=bd_venture;ConnectionTimeout=1";
-                var conexao = new MySqlConnection(strConexao);
-                conexao.Open();
-
-                var comando = new MySqlCommand("SELECT * FROM arroz", conexao);
-                var reader = comando.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    MessageBox.Show($"{reader["id"]} => {reader["nome"]}");
-                }
-                MessageBox.Show("Conexão Ok");
-            }
-            catch (MySqlException ex)
-            {
-                switch (ex.Number)
-                {
-                    case 0:
-                        MessageBox.Show("Não foi possível conectar ao servidor.");
-                        break;
-                    case 1045:
-                        MessageBox.Show("Credenciais de acesso inválidas.");
-                        break;
-                    default:
-                        MessageBox.Show($"Erro MySQL: {ex.Number} - {ex.Message}");
-                        break;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Erro: {ex.Data} - {ex.Message}");
-            }*/
 
         }
 
